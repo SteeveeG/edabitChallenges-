852aee08490eb4ef3dfa08c30469bb18846e4f40
Steve g <stevnkuehnl@gmail.com>
2023-11-26 17:44:57 +0100

    New Challenge Password Validation

---
+++ EdabitChallenges/PasswordValidation/Program.cs
@@ -0,0 +1,31 @@
+﻿//Challenge is to Validate a password with specific rules 
+// link to Challenge : https://edabit.com/challenge/etT7orqDDXJF2zGYM
+
+using PasswordValidation;
+
+var validator = new Validator();
+
+Console.WriteLine("Password Validation");
+
+var passwords = new List<string>();
+//Invallid
+passwords.Add("P1zz@");
+passwords.Add("P1zz@P1zz@P1zz@P1zz@P1zz@");
+passwords.Add("mypassword11");
+passwords.Add("MYPASSWORD11");
+passwords.Add("iLoveYou");
+passwords.Add("Pè7$areLove");
+passwords.Add("Repeeea7!");
+//Valid
+ 
+passwords.Add("H4(k+x0");                
+passwords.Add("Fhg93@");     
+passwords.Add("aA0!@#$%^&*()+=_-{}[]:;\""); 
+passwords.Add("zZ9'?<>,.");
+
+foreach (var password in passwords)
+{
+    Console.WriteLine("\n"+password);
+    Console.WriteLine(validator.ValidatePassword(password));
+    Thread.Sleep(500);
+}
---
+++ EdabitChallenges/PasswordValidation/Validator.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PasswordValidation;
+
+public class Validator
+{
+    Regex upperCaseRegex = new("^[A-Z]+$");
+    Regex lowerCaseRegex = new("^[a-z]+$");
+    Regex AlphabeticRegex = new("^[A-Za-z]+$");
+    Regex numbersRegex = new("^[0-9]+$");
+    Regex NumbersLowerCaseRegex = new("^[a-z0-9]+$");
+    Regex NumbersUpperCaseRegex = new("^[A-Z0-9]+$");
+    Regex SpecialCharactersRegex = new("^[a-zA-Z0-9!@#$%^&*()+=_-{}\\[\\]:;\"'<>?,.-]+$");
+    
+
+
+    public string ValidatePassword(string password)
+    {
+        switch (password.Length)
+        {
+            case > 24:
+                return "too long";
+            case < 6:
+                return "too short";
+        }
+
+        if (upperCaseRegex.IsMatch(password))
+        {
+            return "Missing Numbers and lowercase";
+        }
+  
+        if (lowerCaseRegex.IsMatch(password))
+        {
+            return "Missing Numbers and uppercase";
+        }
+        if (numbersRegex.IsMatch(password))
+        {
+            return "Missing lowercase and uppercase";
+        }
+        if (AlphabeticRegex.IsMatch(password))
+        {
+            return "Missing Numbers";
+        }
+        if (NumbersLowerCaseRegex.IsMatch(password))
+        {
+            return "Missing uppercase";
+        }
+        if (NumbersUpperCaseRegex.IsMatch(password))
+        {
+            return "Missing lowercase";
+        }
+        if (!SpecialCharactersRegex.IsMatch(password))
+        { 
+            return "Special Character not Supported";
+        }
+        for (int i = 0; i < password.Length; i++)
+        {
+            if (password[i] == password[(i + 1) % password.Length] &&
+                password[i] == password[(i + 2) % password.Length])
+            {
+                return "Maximum of 2 repeated characters";
+            }
+        }
+
+        return "OK !";
+    }
+}
